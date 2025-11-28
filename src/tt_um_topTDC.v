4d852f8c76c7c610ea46ea5422d5d5970f9432ca
MariaGrado <al192179@alumnos.uacj.mx>
2025-11-27 18:23:57 -0700

    Update and rename project.v to tt_um_topTDC.v

--- src/tt_um_topTDC.v
+++ src/tt_um_topTDC.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module tt_um_example (
+module tt_um_topTDC (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
@@ -17,11 +17,13 @@ module tt_um_example (
 );
 
   // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
+  //assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
+  //assign uio_out = 0;
+  //assign uio_oe  = 0;
 
-  // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+    topTDC U1 (.clk(clk), .stop(uio_in[7]), .reset(uio_in[6]), .tx(uio_out[1]), .eot(uio_out[0]));
+    assign uio_oe = 8'b00000011;
+    assign uio_out[7:2] = 6'b000000;
+    assign uo_out[7:0] = 8'b00000000;
 
 endmodule
